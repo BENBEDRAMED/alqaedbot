f3debc67581b0c398a2a882d0a84d56b3fe3c8b5
BENBEDRAMED <ma.benbedra@esi-sba.dz>
2025-09-27 16:50:33 +0100

    split to multiple files

---
+++ handlers/init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .admin_handlers import delete_messages, warn_user, mute_user, ban_user
+from .user_handlers import start, help_command, check_reputation, monitor_user, status, test_command, welcome_new_member
+from .message_handlers import detect_controversial, arabic_help, arabic_delete, arabic_warn, arabic_mute, arabic_ban, arabic_rep, arabic_monitor
+
+__all__ = [
+    'delete_messages', 'warn_user', 'mute_user', 'ban_user',
+    'start', 'help_command', 'check_reputation', 'monitor_user', 'status', 'test_command', 'welcome_new_member',
+    'detect_controversial', 'arabic_help', 'arabic_delete', 'arabic_warn', 'arabic_mute', 'arabic_ban', 'arabic_rep', 'arabic_monitor'
+]
